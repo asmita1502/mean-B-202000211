6fdb5e722e050c74637a86eb17d4c57ac4ccbe17
Your Name <you@example.com>
2022-09-29 18:47:32 +0530

    mean-B-202000211

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'My Title';
+  count = 0;
+  number1 =0;
+  number2 =0;
+  result= 0;
+  addition = () => {
+    this.result = this.number1 + this.number2;
+  }
+  listOfNumbers = [12, 25, 33, 45];
+  listOfNames = ["Asmita","Angelica","Krishnananda","Samya","Shatabdi","Sneha"];
+  increment = () => {
+    this.count++;
+  }
+  decrement = () => {
+    this.count--;
+  }
 }
